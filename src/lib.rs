444dabd67ac59bc6050c0fbb8952dfd45e2f356a
abishekk92 <602823+abishekk92@users.noreply.github.com>
2025-03-06 21:17:05 +0530

    remove unused test

--- src/lib.rs
+++ src/lib.rs
@@ -13,25 +13,7 @@ pub use proofs::{StateProof, TokenizedObjectProof};
 pub use storage_traits::{UnitsStorage, UnitsStorageIterator, UnitsStorageProofEngine};
 
 // Re-export the storage implementations
-#[cfg(feature = "sqlite")]
-pub use storage::SqliteStorage;
 #[cfg(feature = "rocksdb")]
 pub use storage::RocksDbStorage;
-
-#[cfg(test)]
-mod tests {
-    use crate::objects::{TokenType, TokenizedObject};
-    use crate::UnitsObjectId;
-
-    #[test]
-    fn it_works() {
-        let token = TokenizedObject {
-            data: vec![1, 2, 3, 4],
-            token_type: TokenType::Native,
-            holder: UnitsObjectId::default(),
-            token_manager: UnitsObjectId::default(),
-            id: UnitsObjectId::default(),
-        };
-        println!("{:?}", token);
-    }
-}
+#[cfg(feature = "sqlite")]
+pub use storage::SqliteStorage;
